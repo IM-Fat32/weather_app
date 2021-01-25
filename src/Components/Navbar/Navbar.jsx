52714d1837f399a3422f905fcefa7a2870b2681e
Dawid Neumann <dawid.neumann.official@gmail.com>
2021-01-25 19:16:04 +0100

    navbar v1.2.1, add resposnive not ready design

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,6 +7,7 @@ import { Icon } from '@iconify/react';
 import calendarDay from '@iconify-icons/bi/calendar-day';
 import calendarWeek from '@iconify-icons/bi/calendar-week';
 import api1 from '@iconify-icons/carbon/api-1';
+import menuLeft from '@iconify-icons/mdi/menu-left';
 
 const Navbar = () => {
   //active menu
@@ -17,12 +18,15 @@ const Navbar = () => {
     setIsActive((prevState) => !prevState);
 
   }
-
   return (
-    <nav style={isActive ? { transform: `translateX(${0}px)` } : { transform: `translateX(${-250}px)` }} >
+    <nav style={
+      isActive ? { transform: `translateX(${0}px)` } : { transform: `translateX(${-250}px)` }}
+    >
       <ul>
         <div className="activator" onClick={handleActiveMenu}>
-
+          <div id="arrow"
+            style={isActive ? { transform: `rotate(${0}deg)` } : { transform: `rotate(${180}deg)` }}>
+            <Icon icon={menuLeft} color="#fff" /></div>
         </div>
         <li>
           <div className="icon"><Icon icon={calendarDay} /></div>
