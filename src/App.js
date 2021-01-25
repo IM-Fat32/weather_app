0a774a4c335e1235eb1f2610764ecac25456677e
Dawid Neumann <dawid.neumann.official@gmail.com>
2021-01-25 16:21:54 +0100

    project structure v.1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 
 function App() {
+  const [lang, setLang] = useState(true);
+
+
   return (
+    <Router>
     <div className="App">
-          Init
+        {/* Maain menu */}
+        <Switch>
+          <Route path="/">
+            <p>Hello</p>
+          </Route>
+        </Switch>
     </div>
+    </Router>
   );
 }
 
